d4699cfdfa52594ad28862f70b6215aef002d783
Sanjeev Budha <budhasanjeev@gmail.com>
2016-06-09 13:27:52 +0545

    document and news added

--- stocky/grails-app/conf/BootStrap.groovy
+++ stocky/grails-app/conf/BootStrap.groovy
@@ -12,7 +12,7 @@ class BootStrap {
         def user = User.findById(1)?:new User(username:'amrit',password:'amrit',firstName:'Amrit',mobileNumber:'9843723189',landLine:'01-4488612',lastName:' Silwal',email:'amrit.silwl@deerwalk.edu.np',userImage:null).save(flush: true)
         def changeDesign = ChangeDesign.findById(1)?:new ChangeDesign(fileName:'logo.png',menuColor:'#226CB5',textColor:'#ffffff',hoverColor:'#0E683B',hoverTextColor:'#ffffff',footerColor:'#226CB5',footerTextColor:'#fff').save(flush: true)
         def adminRole = Role.findByAuthority('ROLE_ADMIN')?:new Role(authority:'ROLE_ADMIN').save(flush: true)
-        def staffRole = Role.findByAuthority('ROLE_STAFF')?:new Role(authority:'ROLE_EMPLOYEE').save(flush: true)
+        def staffRole = Role.findByAuthority('ROLE_EMPLOYEE')?:new Role(authority:'ROLE_EMPLOYEE').save(flush: true)
         def shareholderRole = Role.findByAuthority('ROLE_SHAREHOLDER')?:new Role(authority:'ROLE_SHAREHOLDER').save(flush: true)
         def executiveRole = Role.findByAuthority('ROLE_EXECUTIVE')?:new Role(authority:'ROLE_EXECUTIVE').save(flush: true)
 
@@ -24,6 +24,8 @@ class BootStrap {
         new File("upload/signature").mkdir()
         new File("upload/shareCertificate").mkdir()
         new File("upload/minuteDoc").mkdir()
+        new File("upload/document").mkdir()
+        new File("upload/news").mkdir()
         new File("upload/logo").mkdir()
         new File("report").mkdir()
     }
