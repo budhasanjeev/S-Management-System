d3b51bee48363d4a499d8585bb450ce325f3248e
Sanjeev Budha <budhasanjeev@gmail.com>
2016-06-22 20:03:01 +0545

    updated

--- stocky/grails-app/services/stocky/MinuteService.groovy
+++ stocky/grails-app/services/stocky/MinuteService.groovy
@@ -8,7 +8,8 @@ import org.springframework.web.multipart.commons.CommonsMultipartFile
 @Transactional
 class MinuteService {
 
-    def  imageUploadService
+    def imageUploadService
+    def minuteService
 
   def minuteUpload(params)
   {
@@ -29,4 +30,32 @@ class MinuteService {
           return false
       }
   }
+
+    def minuteUpdate(params){
+
+        println("=="+params)
+        CommonsMultipartFile file = params.myFile
+        String minuteDoc = file.getOriginalFilename()
+
+        def minute = Minute.findById(params.minuteId as Long)
+        if (!minuteDoc.empty){
+            params.minuteDoc = minuteDoc
+            minuteService.minuteUpload(params)
+        }
+        else {
+            params.minuteDoc = minute.minuteDoc
+        }
+
+        minute.properties = params
+
+        if(minute.save(flush: true,failOnError: true)){
+
+            return true
+        }else{
+            return false
+        }
+
+    }
+
+
 }
